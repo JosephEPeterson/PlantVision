c0f3b557f688b3ecf04437bf67e48033d2b09c82
JosephEPeterson <49570602+JosephEPeterson@users.noreply.github.com>
2019-10-01 10:53:07 -0400

    Create AreaOfSeries.m

---
+++ VisionTest1/AreaOfSeries.m
@@ -0,0 +1,18 @@
+close all; clc; clear;
+
+cd Chia/
+I01 = imread('P1.png');
+
+for i=1:13
+    fname = strrep('P1.png', '1', string(i));
+    dummy = imread(fname);
+    
+    hsv = rgb2hsv(dummy);
+    hueImage = hsv(:, :, 1);
+    valueImage = hsv(:, :, 3);
+
+    GreenPixels = hueImage > .177 & hueImage < .306 & valueImage<.8;
+    plantArea(i) = sum(GreenPixels(:));
+end
+
+cd ..
